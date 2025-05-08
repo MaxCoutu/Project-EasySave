c5bb35867522a70dbf87d76baf463b0e517c3563
jérémy maille <jeremy.maille@viacesi.fr>
2025-05-08 18:09:08 +0200

    Initialise Project + Creation of Models + Creation of ViewModels + Creation of ServiceInitial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Projet.Infrastructure;
+using Projet.Service;
+using Projet.View;
+using Projet.ViewModel;
+using System;
+
+namespace Projet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPathProvider paths = new DefaultPathProvider();
+            ILogger logger = new JsonLogger(paths);
+
+            
+            IBackupService backupSvc = new BackupService(logger);
+            ILanguageService langSvc = new JsonLanguageService("Languages/en.json");
+
+            
+            MainViewModel mainVm = new MainViewModel(backupSvc);
+            AddJobViewModel addVm = new AddJobViewModel(backupSvc);
+            RemoveJobViewModel removeVm = new RemoveJobViewModel(backupSvc);
+
+            
+            IAddJobView addView = new ConsoleAddJobView(addVm, langSvc);
+            IRemoveJobView removeView = new ConsoleRemoveJobView(removeVm, backupSvc);
+
+            
+            IMainView mainView = new ConsoleMainView(mainVm, langSvc, addView, backupSvc);
+
+           
+            mainView.Show();
+        }
+    }
+}
---
+++ Service/BackupService.cs
@@ -0,0 +1,65 @@
+﻿using Projet.Infrastructure;
+using Projet.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet.Service
+{
+    public class BackupService: IBackupService
+    {
+        public event Action<StatusEntry> StatusUpdated;
+
+        private readonly ILogger _logger;
+        private readonly List<BackupJob> _jobs = new List<BackupJob>();
+        private const int MaxJobs = 5;
+
+        public BackupService(ILogger logger) => _logger = logger;
+
+        
+        public void AddBackup(BackupJob job)
+        {
+            if (_jobs.Count >= MaxJobs)
+                throw new InvalidOperationException("Maximum number of backup jobs reached (5).");
+
+            _jobs.Add(job);
+        }
+
+        public void RemoveBackup(string name) => _jobs.RemoveAll(j => j.Name == name);
+
+        public IReadOnlyList<BackupJob> GetJobs() => _jobs.AsReadOnly();
+
+        
+        public async Task ExecuteBackupAsync(string name)
+        {
+            BackupJob job = _jobs.First(j => j.Name == name);
+
+            Report(new StatusEntry(job.Name, "", "", "PENDING", 0, 0, 0, 0));
+
+            DateTime start = DateTime.UtcNow;
+            await job.RunAsync(Report);
+            DateTime end = DateTime.UtcNow;
+
+            Report(new StatusEntry(job.Name, "", "", "END", 0, 0, 0, 1));
+
+            _logger.LogEvent(new LogEntry(
+                end, job.Name, job.SourceDir, job.TargetDir,
+                0, (int)(end - start).TotalMilliseconds));
+        }
+
+        public async Task ExecuteAllBackupsAsync()
+        {
+            foreach (BackupJob job in _jobs)
+                await ExecuteBackupAsync(job.Name);
+        }
+
+        
+        private void Report(StatusEntry entry)
+        {
+            _logger.UpdateStatus(entry);
+            StatusUpdated?.Invoke(entry);
+        }
+    }
+}
