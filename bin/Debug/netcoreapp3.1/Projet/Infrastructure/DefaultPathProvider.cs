e5dff4a062b7cf491ec8736a11e6b0d1093560d9
jérémy maille <jeremy.maille@viacesi.fr>
2025-05-08 18:22:43 +0200

    add missing config files

---
+++ bin/Debug/netcoreapp3.1/Projet/Infrastructure/DefaultPathProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Projet.Infrastructure
+{
+    public class DefaultPathProvider: IPathProvider
+    {
+        private static readonly string BaseDir =
+            @"C:\Projet";
+
+        public string GetLogDir()
+        {
+            string dir = Path.Combine(BaseDir, "Logs");
+            Directory.CreateDirectory(dir);
+            return dir;
+        }
+
+        public string GetStatusDir()
+        {
+            string dir = Path.Combine(BaseDir, "Status");
+            Directory.CreateDirectory(dir);
+            return dir;
+        }
+    }
+}
---
+++ bin/Debug/netcoreapp3.1/Projet/Service/JsonLanguageService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+
+namespace Projet.Service
+{
+    public class JsonLanguageService : ILanguageService
+    {
+        private readonly Dictionary<string, string> _dict;
+
+        public JsonLanguageService(string jsonPath)
+        {
+            _dict = File.Exists(jsonPath)
+                ? JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText(jsonPath))
+                    ?? new Dictionary<string, string>()
+                : new Dictionary<string, string>();
+        }
+
+        public string Translate(string key) =>
+            _dict.TryGetValue(key, out string value) ? value : key;
+    }
+}
